010990665015fdd40915d0b5d2d68b5a35d16b52
Simon Barth <sb@b1t.at>
2026-04-15 20:06:09 +0200

    Add a bunch of tests for task config parsing

--- machfile/src/raw_config.rs
+++ machfile/src/raw_config.rs
@@ -109,3 +109,229 @@ impl RawTaskOptions {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env::current_dir;
+
+    use super::*;
+
+    #[test]
+    fn raw_task_options_parse_correctly_with_minimal_data() {
+        let opts = RawTaskOptions {
+            working_directory: None,
+            environment: None,
+        };
+        let path = Path::new("/tmp");
+
+        let result = opts.parse(&path);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().working_directory.as_path(), path);
+    }
+
+    #[test]
+    fn raw_task_options_constructs_relative_working_dir_correctly() {
+        let opts = RawTaskOptions {
+            working_directory: Some("src".to_string()),
+            environment: None,
+        };
+        let calling_directory_buf = current_dir().unwrap();
+        let mut final_directory_buf = calling_directory_buf.clone();
+
+        let path = calling_directory_buf.as_path();
+
+        final_directory_buf.push("src");
+
+        let result = opts.parse(&path);
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().working_directory, final_directory_buf);
+    }
+
+    #[test]
+    fn raw_task_options_errors_with_non_directory_as_working_dir() {
+        let opts = RawTaskOptions {
+            working_directory: Some("README.md".to_string()),
+            environment: None,
+        };
+        let calling_directory_buf = current_dir().unwrap();
+        let path = calling_directory_buf.as_path();
+
+        let result = opts.parse(&path);
+        assert!(result.is_err());
+
+        let error = result.unwrap_err();
+        assert_eq!(
+            error,
+            ConfigParseError::InvalidTaskDefinition("Invalid working_directory".to_string())
+        );
+    }
+
+    #[test]
+    fn raw_task_options_errors_with_non_existing_path_as_working_dir() {
+        let opts = RawTaskOptions {
+            working_directory: Some("this_path_should_not_exist_in_the_repo".to_string()),
+            environment: None,
+        };
+        let calling_directory_buf = current_dir().unwrap();
+        let path = calling_directory_buf.as_path();
+
+        let result = opts.parse(&path);
+        assert!(result.is_err());
+
+        let error = result.unwrap_err();
+        assert_eq!(
+            error,
+            ConfigParseError::InvalidTaskDefinition("Invalid working_directory".to_string())
+        );
+    }
+
+    #[test]
+    fn raw_task_options_constructs_absolute_dir_correctly() {
+        let opts = RawTaskOptions {
+            working_directory: Some("/tmp".to_string()),
+            environment: None,
+        };
+        let calling_directory_buf = current_dir().unwrap();
+        let path = calling_directory_buf.as_path();
+
+        let result = opts.parse(&path);
+        assert!(result.is_ok());
+        assert_eq!(
+            result.unwrap().working_directory.as_path(),
+            Path::new("/tmp")
+        );
+    }
+
+    #[test]
+    fn raw_task_options_handles_environment_dict_well() {
+        let mut test_map = HashMap::new();
+        test_map.insert("ALPHA".to_string(), "alpha_val".to_string());
+        test_map.insert("BRAVO".to_string(), "bravo_val".to_string());
+
+        let opts = RawTaskOptions {
+            working_directory: None,
+            environment: Some(test_map),
+        };
+        let calling_directory_buf = current_dir().unwrap();
+        let path = calling_directory_buf.as_path();
+
+        let result = opts.parse(&path);
+        assert!(result.is_ok());
+
+        let env_map = result.unwrap().environment;
+        assert!(!env_map.is_empty());
+        assert!(env_map.contains_key("ALPHA"));
+        assert!(env_map.contains_key("BRAVO"));
+        assert_eq!(env_map.get("ALPHA"), Some(&"alpha_val".to_string()));
+        assert_eq!(env_map.get("BRAVO"), Some(&"bravo_val".to_string()));
+    }
+
+    #[test]
+    fn raw_task_config_parses_correctly_with_minimal_data() {
+        let raw_task = RawTaskConfig {
+            script: Some("touch".to_string()),
+            desc: None,
+            deps: None,
+            options: None,
+        };
+        let result = raw_task.parse("name", Path::new("/tmp"));
+
+        assert!(result.is_ok());
+        let task = result.unwrap();
+        assert_eq!(task.name, "name".to_string());
+        assert!(task.script.is_some());
+        let script = task.script.unwrap();
+        assert_eq!(script.len(), 1);
+        assert_eq!(script[0].command, "touch".to_string());
+        assert_eq!(script[0].args.len(), 0);
+    }
+
+    #[test]
+    fn raw_task_config_parses_script_with_single_command_single_arg_correctly() {
+        let raw_task = RawTaskConfig {
+            script: Some("touch me".to_string()),
+            desc: None,
+            deps: None,
+            options: None,
+        };
+        let result = raw_task.parse("name", Path::new("/tmp"));
+
+        assert!(result.is_ok());
+        let task = result.unwrap();
+        assert!(task.script.is_some());
+        let script = task.script.unwrap();
+        assert_eq!(script.len(), 1);
+        assert_eq!(script[0].command, "touch".to_string());
+        assert_eq!(script[0].args.len(), 1);
+        assert_eq!(script[0].args[0], "me".to_string());
+    }
+
+    #[test]
+    fn raw_task_config_parses_script_with_single_command_multi_arg_correctly() {
+        let raw_task = RawTaskConfig {
+            script: Some("touch me one more --time".to_string()),
+            desc: None,
+            deps: None,
+            options: None,
+        };
+        let result = raw_task.parse("name", Path::new("/tmp"));
+
+        assert!(result.is_ok());
+        let task = result.unwrap();
+        assert!(task.script.is_some());
+        let script = task.script.unwrap();
+        assert_eq!(script.len(), 1);
+        assert_eq!(script[0].command, "touch".to_string());
+        assert_eq!(script[0].args.len(), 4);
+        assert_eq!(script[0].args[0], "me".to_string());
+        assert_eq!(script[0].args[1], "one".to_string());
+        assert_eq!(script[0].args[2], "more".to_string());
+        assert_eq!(script[0].args[3], "--time".to_string());
+    }
+
+    #[test]
+    fn raw_task_config_parses_script_with_multiple_simple_commands_correctly() {
+        let raw_task = RawTaskConfig {
+            script: Some("touch\nwho".to_string()),
+            desc: None,
+            deps: None,
+            options: None,
+        };
+        let result = raw_task.parse("name", Path::new("/tmp"));
+
+        assert!(result.is_ok());
+        let task = result.unwrap();
+        assert!(task.script.is_some());
+        let script = task.script.unwrap();
+        assert_eq!(script.len(), 2);
+        assert_eq!(script[0].command, "touch".to_string());
+        assert_eq!(script[1].command, "who".to_string());
+    }
+
+    #[test]
+    fn raw_task_config_parses_script_with_multi_command_multi_arg_correctly() {
+        let raw_task = RawTaskConfig {
+            script: Some("touch me one\nmore --time".to_string()),
+            desc: None,
+            deps: None,
+            options: None,
+        };
+        let result = raw_task.parse("name", Path::new("/tmp"));
+
+        assert!(result.is_ok());
+        let task = result.unwrap();
+        assert!(task.script.is_some());
+        let script = task.script.unwrap();
+        assert_eq!(script.len(), 2);
+
+        // First command
+        assert_eq!(script[0].command, "touch".to_string());
+        assert_eq!(script[0].args.len(), 2);
+        assert_eq!(script[0].args[0], "me".to_string());
+        assert_eq!(script[0].args[1], "one".to_string());
+        // Second command
+        assert_eq!(script[1].command, "more".to_string());
+        assert_eq!(script[1].args.len(), 1);
+        assert_eq!(script[1].args[0], "--time".to_string());
+    }
+}
